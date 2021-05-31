43fedd03958189362f7cfbcebb520f356eed2bc8
Natalia Ramirez <natroram@espol.edu.ec>
2021-05-31 11:05:19 -0500

    genero conflicto c:

--- JavaApplication/src/javaapplication/JavaApplication.java
+++ JavaApplication/src/javaapplication/JavaApplication.java
@@ -16,10 +16,10 @@ public class JavaApplication {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World");
+        System.out.println("Hello World - soy un conflicto");
         
         for(int i = 0; i<10; i++){
-            System.out.println(i);
+            System.out.println(i+5);
         }
     }
     
